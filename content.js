ec01d6096a8433f9563099c4de64266070422693
Alfred <alfredboahene.7@gmail.com>
2026-02-15 23:32:23 -0600

    add logo

--- content.js
+++ content.js
@@ -66,8 +66,9 @@ function createShieldUI(targetElement) {
     // Create inner logo
     const logo = document.createElement('div');
     logo.className = 'ps-extension-logo';
-    // White shield icon
-    logo.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path></svg>';
+    // Use logo.png instead of SVG
+    const logoUrl = chrome.runtime.getURL('logo.png');
+    logo.innerHTML = `<img src="${logoUrl}" width="16" height="16" style="display:block;">`;
     
     // Create status text (checkmark or count)
     const statusText = document.createElement('span');
